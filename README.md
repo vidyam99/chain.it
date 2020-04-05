c423c733045763cf6bf94929fc2fe8f9aaf01ead
vidyam99 <vidyajmakwana@gmail.com>
2020-04-06 02:51:41 +0530

    webapp added

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# chain.it
+
+<h1 align="center">CHAIN.IT</h1>
+
+Small and Medium business contribute to over 80% of the total payments transactions and financial industry is committed to help these businesses to nurture and support local economies at scale. However, there are many difficulties involved in their end to end processes. This makes it important to leverage use of technology available to help them come together and flourish.
+
+### Goals of this project:
+
+* [x] To create a complete decentralised network with no data dominance.
+* [x] To secure transactions and minimize frauds.
+* [x] To automate the inventory and supply chain management through Smart Contracts.
+* [x] To highlight useful insights by real time data anaysis.
+
+### Future scope of this project:
+
+* [ ] Location can be recorded for every transaction to trace the frauds if any.
+* [ ] Offline working of the nodes.
+* [ ] Provide Notary services.
+
+#### The project can be run by following the steps below:
+* Clone this [repo](https://github.com/vidyam99/chain.it):
+	* `git clone https://github.com/vidyam99/chain.it.git`
+* Navigate to the folder:
+  * `cd chain.it/WebApp`
+* Install the requirements:
+  * `pip install -r requirements.txt`
+* Run the Flask WebApp. Create multiple instances of the server by varying the port number:
+  * `flask run --port 5000`
+  * `flask run --port 5001`
+* Enter the localhost url into web browser:
+  * `http://127.0.0.1:5000`
+  * `http://127.0.0.1:5001`
+
+<h3 align="center"><b>Developed by <a href="https://github.com/vidyam99">Vidya Makwana</a> and <a href="https://github.com/sayali-khot">Sayali Khot</a> with Blockchain, Flask and :heart:.</b></h1>
---
+++ WebApp/blockchain.py
@@ -0,0 +1,161 @@
+from hashlib import sha256
+import json
+import time
+
+import Crypto
+import Crypto.Random
+from Crypto.Hash import SHA
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+import binascii
+from collections import OrderedDict
+
+MINING_SENDER = "CHAIN.IT"
+
+class Block:
+    def __init__(self, index, transactions, timestamp, previous_hash, nonce=0):
+        self.index = index
+        self.transactions = transactions
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash
+        self.nonce = nonce
+
+    def compute_hash(self):
+        block_string = json.dumps(self.__dict__, sort_keys=True)
+        return sha256(block_string.encode()).hexdigest()
+
+
+class Blockchain:
+    difficulty = 2
+
+    def __init__(self):
+        self.unconfirmed_transactions = []
+        self.chain = []
+
+    def create_genesis_block(self):
+        genesis_block = Block(0, [], "April Fools", "0")
+        genesis_block.hash=genesis_block.compute_hash()
+        self.chain.append(genesis_block)
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    @property
+    def last_transaction(self):
+        return self.unconfirmed_transactions[-1]
+
+    def add_block(self, block, proof):
+        previous_hash = self.last_block.hash
+
+        if previous_hash != block.previous_hash:
+            return False
+
+        if not Blockchain.is_valid_proof(block, proof):
+            return False
+
+        block.hash = proof
+        self.chain.append(block)
+        self.unconfirmed_transactions = []
+        return True
+
+    @staticmethod
+    def proof_of_work(block):
+        block.nonce = 0
+
+        computed_hash = block.compute_hash()
+        while not computed_hash.startswith('0' * Blockchain.difficulty):
+            block.nonce += 1
+            computed_hash = block.compute_hash()
+
+        return computed_hash
+
+    def add_new_transaction(self, transaction):
+        self.unconfirmed_transactions.append(transaction)
+
+    @classmethod
+    def is_valid_proof(cls, block, block_hash):
+        return (block_hash.startswith('0' * Blockchain.difficulty) and
+                block_hash == block.compute_hash())
+
+    @classmethod
+    def check_chain_validity(cls, chain):
+        result = True
+        previous_hash = "0"
+
+        for block in chain:
+            block_hash = block.hash
+            delattr(block, "hash")
+
+            if not cls.is_valid_proof(block, block.hash) or previous_hash != block.previous_hash:
+                result = False
+                break
+
+            block.hash, previous_hash = block_hash, block_hash
+
+        return result
+
+    def mine(self):
+        if not self.unconfirmed_transactions:
+            return False
+
+        last_block = self.last_block
+
+        new_block = Block(index=last_block.index + 1,
+                          transactions=self.unconfirmed_transactions,
+                          timestamp=time.ctime(),
+                          previous_hash=last_block.hash)
+
+        proof = self.proof_of_work(new_block)
+        self.add_block(new_block, proof)
+
+        # self.unconfirmed_transactions = []
+
+        return True
+
+    def verify_transaction_signature(self, sender_address, signature, transaction):
+        public_key = RSA.importKey(binascii.unhexlify(sender_address))
+        verifier = PKCS1_v1_5.new(public_key)
+        h = SHA.new(str(transaction).encode('utf8'))
+        return verifier.verify(h, binascii.unhexlify(signature))
+
+
+    def submit_transaction(self, sender_address, recipient_address, value, signature):
+        transaction = OrderedDict({'sender_address': sender_address,
+                                    'recipient_address': recipient_address,
+                                    'value': value})
+        #Reward for mining a block
+        if sender_address == MINING_SENDER:
+            # self.unconfirmed_transactions.append(transaction)
+            return True
+        #Manages transactions from wallet to another wallet
+        else:
+            transaction_verification = self.verify_transaction_signature(sender_address, signature, transaction)
+            if transaction_verification:
+                # transaction['timestamp'] = time.ctime()
+                # self.unconfirmed_transactions.append(transaction)
+                return True
+            else:
+                return False
+
+class Transaction:
+
+    def __init__(self, sender_address, sender_private_key, recipient_address, value):
+        self.sender_address = sender_address
+        self.sender_private_key = sender_private_key
+        self.recipient_address = recipient_address
+        self.value = value
+
+    def __getattr__(self, attr):
+        return self.data[attr]
+
+    def to_dict(self):
+        return OrderedDict({'sender_address': self.sender_address,
+                            'recipient_address': self.recipient_address,
+                            'value': self.value})
+
+    def sign_transaction(self):
+        private_key = RSA.importKey(binascii.unhexlify(self.sender_private_key))
+        signer = PKCS1_v1_5.new(private_key)
+        h = SHA.new(str(self.to_dict()).encode('utf8'))
+        return binascii.hexlify(signer.sign(h)).decode('ascii')
